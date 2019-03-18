45cb217d2fabc3d8c8888b299133f37532962da6
ckx <303813367@qq.com>
2019-03-18 11:24:41 +0800

    修改敏感词

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,86 @@
-< template >  < div class = "index-wrap" >  < h1 class = "header" > 编程技术分享 </h1 >  < div class = "index-main" >  < ul class = "main-list" >  < li class = "main-item" >  < h3 class = "main-item-title" > 如何改善国内前端开发行业的尴尬地位？ </h3 >  < div
-            class = "main-item-cont" > ……1. 前情提要 毋庸讳言，在我刚工作的时候，前端是还是一个不受重视的岗位。切图狗，写网页的这种侮辱性绰号绑在前端开发身上。我自己体验到的一个很明显的标志是：招聘网站上，前端开发的工资是其他开发工资的……90 % 甚至更低。 在知乎一个2011年的前端地位问题（《如何改善国内前端开发行业的尴尬地位？》）中，包括贺师俊老师都承认这种尴尬确实存在。但是，30年河东30年河西，到了2018年，前端已经成了炽手可……热的岗位。现在如果开一家科技公司或者组建自己的一个团队，要是还没有自己的前端，那我只能说你这家公司还不上台面。包括最近今日头条和拼多多在疯狂招人，前端也是最需要的岗位之一。 究竟是什么原因，导致前端在…… </div >  </li >  < li class = "main-item" >  < h3 class = "main-item-title" > 你以为你真的了解Web前端开发吗？未来前端远比你想的有前途！ </h3 >  < div
-            class = "main-item-cont" > ……近几年来，随着 HTML5、JS 的流行，前端这个职业火热了起来！不少人发出疑惑，前端以后还会更有前途吗？小编整理的一整套系统的web前端学习教程，关注薇信公众号：程序员大牛，回复 {前端即可免费领取……！ 我只能告诉你：前端不灭 现在都明白了用户体验至上，还要用着舒服 后端提供床，前端提供颜值高的妹，你说重要吗？ 我来简单的介绍下web前端开发的3个疑问 ｛“ web前端开发 ”是什么……？ ｝ ｛“ web前端开发 ”有前途吗？｝ ｛“ web前端开发 ”到底怎么学？｝ NO 1： web前端开发 ”是什么？ 在这里我还是要推荐下我自己建的web前端开发学习群：731669587，群里都是学web…… </div >  </li >  </ul >  </div >  < div class = "footer" > 版权 所有 </div >  </div >  </template >  < script > 
+<template>
+  <div class="index-wrap">
+    <h1 class="header">编程技术分享</h1>
+    <div class="index-main">
+      <ul class="main-list">
+        <li class="main-item">
+          <h3 class="main-item-title">如何改善国内前端开发行业的尴尬地位？</h3>
+          <div
+            class="main-item-cont"
+          >……1. 前情提要 毋庸讳言，在我刚工作的时候，前端是还是一个不受重视的岗位。切图狗，写网页的这种侮辱性绰号绑在前端开发身上。我自己体验到的一个很明显的标志是：招聘网站上，前端开发的工资是其他开发工资的……90%甚至更低。 在知乎一个2011年的前端地位问题（《如何改善国内前端开发行业的尴尬地位？》）中，包括贺师俊老师都承认这种尴尬确实存在。但是，30年河东30年河西，到了2018年，前端已经成了炽手可……热的岗位。现在如果开一家科技公司或者组建自己的一个团队，要是还没有自己的前端，那我只能说你这家公司还不上台面。包括最近今日头条和拼多多在疯狂招人，前端也是最需要的岗位之一。 究竟是什么原因，导致前端在……</div>
+        </li>
+        <li class="main-item">
+          <h3 class="main-item-title">你以为你真的了解Web前端开发吗？未来前端远比你想的有前途！</h3>
+          <div
+            class="main-item-cont"
+          >……近几年来，随着 HTML5、JS 的流行，前端这个职业火热了起来！不少人发出疑惑，前端以后还会更有前途吗？小编整理的一整套系统的web前端学习教程，关注薇信公众号：程序员大牛，回复{前端即可免费领取……！ 我只能告诉你：前端不灭 现在都明白了用户体验至上，还要用着舒服 后端提供床，前端提供颜值高的妹，你说重要吗？ 我来简单的介绍下web前端开发的3个疑问 ｛“ web前端开发 ”是什么……？ ｝ ｛“ web前端开发 ”有前途吗？｝ ｛“ web前端开发 ”到底怎么学？｝ NO 1： web前端开发 ”是什么？ 在这里我还是要推荐下我自己建的web前端开发学习群：731669587，群里都是学web……</div>
+        </li>
+      </ul>
+    </div>
+    <div class="footer">版权 所有</div>
+  </div>
+</template>
+
+<script>
 export default {
-  name:'HelloWorld', 
+  name: 'HelloWorld',
   data () {
     return {
 
     }
   }
-} </script >  < ! --Add "scoped" attribute to limit CSS to this component only-- >  < style scoped > 
-.index - wrap {
-  font - size:14px; 
-  font - family:"microsoft yahei"; 
-  background - color:#f8f8f8; 
-  margin:0; 
-  padding:0; 
 }
-.index - main {
-  width:1200px; 
-  margin:0 auto; 
-  background - color:#ffffff; 
-  text - align:left; 
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.index-wrap {
+  font-size: 14px;
+  font-family: "microsoft yahei";
+  background-color: #f8f8f8;
+  margin: 0;
+  padding: 0;
+}
+.index-main {
+  width: 1200px;
+  margin: 0 auto;
+  background-color: #ffffff;
+  text-align: left;
 }
 .header {
-  margin:0; 
-  padding:20px 0; 
-  text - align:center; 
-  font - size:26px; 
-  font - weight:normal; 
+  margin: 0;
+  padding: 20px 0;
+  text-align: center;
+  font-size: 26px;
+  font-weight: normal;
 }
 .footer {
-  margin:0; 
-  padding:20px 0; 
-  text - align:center; 
-  background - color:#ccc; 
-  color:#fff; 
+  margin: 0;
+  padding: 20px 0;
+  text-align: center;
+  background-color: #ccc;
+  color: #fff;
 }
-ul, 
+ul,
 li {
-  list - style:none; 
+  list-style: none;
 }
 a {
-  text - decoration:none; 
+  text-decoration: none;
 }
-.main - list {
-  padding:30px; 
+.main-list {
+  padding: 30px;
 }
-.main - item {
-  padding:20px 0; 
+.main-item {
+  padding: 20px 0;
 }
-.main - item - title {
-  padding:15px 0; 
-  font - size:22px; 
-  line - height:40px; 
+.main-item-title {
+  padding: 15px 0;
+  font-size: 22px;
+  line-height: 40px;
 }
-.main - item - cont {
-  font - size:16px; 
-  line - height:28px; 
-} </style > 
+.main-item-cont {
+  font-size: 16px;
+  line-height: 28px;
+}
+</style>
--- static/test/sensitive-words/js/pdf/index.js
+++ static/test/sensitive-words/js/pdf/index.js
@@ -1,174 +1,177 @@
-;"use strict";
+;
+"use strict";
 // 定义全局变量，高亮敏感词方法，需要在viewer.js中调用该方法
 // 当pdf页面改变时，就需要调用此方法
-var myHighlightSensitiveWords;
-;$(function(){
-	console.log('加载pdfjs插件扩展');
-	//	console.log(pdfjsLib);
-	//	
-	//	console.log(document.readyState);
-	
-	// 定义所有的敏感词
-	var allSensitiveWords = [{
-		id:1,
-		word:'number',
-		discription: '描述描述11111111111',
-		order: '二级'
-	}, {
-		id: 2,
-		word:'are',
-		discription: '描述描述21212131111122222',
-		order: '一级'
-	}, {
-		id: 3,
-		word:'used',
-		discription: '描述描述3333333333333333333',
-		order: '三级'
-	}, {
-		id: 4,
-		word:'is',
-		discription: '描述描述444444444',
-		order: '一级'
-	}, {
-		id: 5,
-		word:'with',
-		discription: '描述描述51231312312',
-		order: '二级'
-	}];
-	// 定义当前pdf页码
-	var currentPageNum = 1; 
-	// 定义当前页拥有的敏感词
-	var currentPageSensitiveWords = [];
-	// 定义当前页的文档内容
-	var currentPageTxt = '';
-	// 敏感词弹窗是否显示，默认不显示
-	var sensitiveWordsLayerIsShow = false;
-	
-	/**
-	 * 获取当前页的文档内容
-	 **/
-	var getCurrentPageTxt = function(){
-		currentPageTxt = $('#viewer').find('[data-page-number='+ currentPageNum +']').find('.textLayer').text();
-	}
-	
-	/**
-	 * 获取当前页拥有的敏感词
-	 **/
-	var getCurrentPageSensitiveWords = function(){
-		for(var i=0; i<allSensitiveWords.length; i++){
-			if(currentPageTxt.indexOf(allSensitiveWords[i].word) != -1){
-				currentPageSensitiveWords.push(allSensitiveWords[i]);
-			}
-		}
-//		console.log(currentPageSensitiveWords);
-	}
-	
-	/**
-	 * 清空pdf所有高亮显示的敏感词
-	 **/
-	var removePdfHighlight = function(){
-		 $('#viewer').find('.highlight').removeClass('highlight');
-	}
-	
-	/**
-	 * 当前页对敏感词进行高亮
-	 **/
-	var setCurrentPageHighlight = function(){
-		 for(var i=0; i<currentPageSensitiveWords.length; i++){
-		 	$('#viewer').find('[data-page-number='+ currentPageNum +']').find('.textLayer').highlight(currentPageSensitiveWords[i].word);
-		 }
-	}
-	
-	/**
-	 * 设置敏感词列表弹窗的dom片段
-	 **/
-	var setSensitiveWordsLayerDom = function(){
-		var htmlPart = '';
-		var htmlItemPart = '';
-		if(currentPageSensitiveWords.length === 0){
-			htmlPart = '<div class="sensitive-words-empty">无</div>';
-		}else{
-			htmlPart += '<ol class="sensitive-words-list">';
-			for(var i=0; i<currentPageSensitiveWords.length; i++){
-				htmlItemPart = '<li class="sensitive-words-item">\
+var myHighlightSensitiveWords;;
+$(function () {
+  console.log('加载pdfjs插件扩展');
+  //	console.log(pdfjsLib);
+  //	
+  //	console.log(document.readyState);
+
+  // 定义所有的敏感词
+  var allSensitiveWords = [{
+    id: 1,
+    word: 'number',
+    discription: '描述描述11111111111',
+    order: '二级'
+  }, {
+    id: 2,
+    word: 'are',
+    discription: '描述描述21212131111122222',
+    order: '一级'
+  }, {
+    id: 3,
+    word: 'used',
+    discription: '描述描述3333333333333333333',
+    order: '三级'
+  }, {
+    id: 4,
+    word: 'is',
+    discription: '描述描述444444444',
+    order: '一级'
+  }, {
+    id: 5,
+    word: 'with',
+    discription: '描述描述51231312312',
+    order: '二级'
+  }];
+  // 定义当前pdf页码
+  var currentPageNum = 1;
+  // 定义当前页拥有的敏感词
+  var currentPageSensitiveWords = [];
+  // 定义当前页的文档内容
+  var currentPageTxt = '';
+  // 敏感词弹窗是否显示，默认不显示
+  var sensitiveWordsLayerIsShow = false;
+
+  /**
+   * 获取当前页的文档内容
+   **/
+  var getCurrentPageTxt = function () {
+    currentPageTxt = $('#viewer').find('[data-page-number=' + currentPageNum + ']').find('.textLayer').text();
+  }
+
+  /**
+   * 获取当前页拥有的敏感词
+   **/
+  var getCurrentPageSensitiveWords = function () {
+    for (var i = 0; i < allSensitiveWords.length; i++) {
+      if (currentPageTxt.indexOf(allSensitiveWords[i].word) != -1) {
+        currentPageSensitiveWords.push(allSensitiveWords[i]);
+      }
+    }
+    //		console.log(currentPageSensitiveWords);
+  }
+
+  /**
+   * 清空pdf所有高亮显示的敏感词
+   **/
+  var removePdfHighlight = function () {
+    $('#viewer').find('.highlight').removeClass('highlight');
+  }
+
+  /**
+   * 当前页对敏感词进行高亮
+   **/
+  var setCurrentPageHighlight = function () {
+    for (var i = 0; i < currentPageSensitiveWords.length; i++) {
+      $('#viewer').find('[data-page-number=' + currentPageNum + ']').find('.textLayer').highlight(currentPageSensitiveWords[i].word);
+    }
+  }
+
+  /**
+   * 设置敏感词列表弹窗的dom片段
+   **/
+  var setSensitiveWordsLayerDom = function () {
+    var htmlPart = '';
+    var htmlItemPart = '';
+    if (currentPageSensitiveWords.length === 0) {
+      htmlPart = '<div class="sensitive-words-empty">无</div>';
+    } else {
+      htmlPart += '<ol class="sensitive-words-list">';
+      for (var i = 0; i < currentPageSensitiveWords.length; i++) {
+        htmlItemPart = '<li class="sensitive-words-item">\
 				        			<div class="sensitive-words-base">\
-				        				<span class="word-wrap">敏感词：<span class="word">'+ currentPageSensitiveWords[i].word +'</span></span>\
-				        				<span class="order-wrap">敏感词级别：<span class="order">'+ currentPageSensitiveWords[i].order +'</span></span>\
+				        				<span class="word-wrap">敏感词：<span class="word">' + currentPageSensitiveWords[i].word + '</span></span>\
+				        				<span class="order-wrap">敏感词级别：<span class="order">' + currentPageSensitiveWords[i].order + '</span></span>\
 				        			</div>\
 				        			<div class="sensitive-words-info">编辑提示：建议使用“残疾人”。请确认是否仍然使用该词。</div>\
 				        		</li>';
-	        	htmlPart += htmlItemPart;
-			}
-			
-			htmlPart += '</ol>';
-		}
-		
-		$('#sensitive-words-layer-main').empty().html(htmlPart);
-		$('#sensitive-words-pagenum').empty().text(currentPageNum);
-	}
-	
-	/**
-	 * 定义高亮当前页所有敏感词
-	 * @param pn {Number} 当前页码
-	 **/
-	myHighlightSensitiveWords = function(pn){
-		// 设置当前页码
-		currentPageNum = parseInt(pn);
-		// 重置当前页拥有的敏感词为空
-		currentPageSensitiveWords = [];
-		// 重置当前页的pdf文档为空
-		currentPageTxt = '';
-		// 设置当前页的pdf文档
-		getCurrentPageTxt();
-		// 设置当前页拥有的敏感词
-		getCurrentPageSensitiveWords();
-		
-		// 当敏感词列表弹窗显示时,进行的操作
-		if(sensitiveWordsLayerIsShow){
-			// 清空pdf所有的高亮显示
-			removePdfHighlight();
-			// 当前页对敏感词进行高亮
-			setCurrentPageHighlight();
-			// 设置敏感词列表弹窗的dom片段
-			setSensitiveWordsLayerDom();
-		}
-		
-		
-	}
-	
-	/**
-	 * 敏感词按钮,绑定事件,控制敏感词列表弹窗显示和隐藏
-	 **/
-	$('#sensitive-word-btn').on('click', function(){
-		var $sensitiveWordsLayer = $('#sensitive-words-layer');
-		// 清空pdf所有的高亮显示
-		removePdfHighlight();
-		// 打开敏感词列表弹窗
-		if(sensitiveWordsLayerIsShow === false){
-			// 当前页对敏感词进行高亮
-			setCurrentPageHighlight();
-			// 设置敏感词列表弹窗的dom片段
-			setSensitiveWordsLayerDom();
-			// 打开敏感词列表弹窗
-			$sensitiveWordsLayer.show();
-		}else{
-			// 关闭弹窗
-			// 关闭敏感词列表弹窗
-			$sensitiveWordsLayer.hide();
-		}
-		
-		sensitiveWordsLayerIsShow = !sensitiveWordsLayerIsShow;
-	})
-	
-	/**
-	 * 敏感词列表弹窗关闭按钮点击事件,关闭弹窗
-	 **/
-	$('#sensitive-words-close-btn').on('click', function(){
-		var $sensitiveWordsLayer = $('#sensitive-words-layer');
-		// 清空pdf所有的高亮显示
-		removePdfHighlight();
-		// 关闭敏感词列表弹窗
-		$sensitiveWordsLayer.hide();
-		sensitiveWordsLayerIsShow = false;
-	})
+        htmlPart += htmlItemPart;
+      }
+
+      htmlPart += '</ol>';
+    }
+
+    $('#sensitive-words-layer-main').empty().html(htmlPart);
+    $('#sensitive-words-pagenum').empty().text(currentPageNum);
+  }
+
+  /**
+   * 定义高亮当前页所有敏感词
+   * @param pn {Number} 当前页码
+   **/
+  myHighlightSensitiveWords = function (pn) {
+    // 设置当前页码
+    currentPageNum = parseInt(pn);
+    // 重置当前页拥有的敏感词为空
+    currentPageSensitiveWords = [];
+    // 重置当前页的pdf文档为空
+    currentPageTxt = '';
+    // 设置当前页的pdf文档
+    getCurrentPageTxt();
+    // 设置当前页拥有的敏感词
+    getCurrentPageSensitiveWords();
+
+    // 当敏感词列表弹窗显示时,进行的操作
+    if (sensitiveWordsLayerIsShow) {
+      // 清空pdf所有的高亮显示
+      removePdfHighlight();
+      // 当前页对敏感词进行高亮
+      setCurrentPageHighlight();
+      // 设置敏感词列表弹窗的dom片段
+      setSensitiveWordsLayerDom();
+    }
+
+
+  }
+
+  /**
+   * 敏感词按钮,绑定事件,控制敏感词列表弹窗显示和隐藏
+   **/
+  $('#sensitive-word-btn').on('click', function () {
+    var $sensitiveWordsLayer = $('#sensitive-words-layer');
+    // 清空pdf所有的高亮显示
+    removePdfHighlight();
+    // 打开敏感词列表弹窗
+    if (sensitiveWordsLayerIsShow === false) {
+      // 重新获取当前页的敏感词
+      myHighlightSensitiveWords(currentPageNum);
+      // 当前页对敏感词进行高亮
+      setCurrentPageHighlight();
+      // 设置敏感词列表弹窗的dom片段
+      setSensitiveWordsLayerDom();
+      // 打开敏感词列表弹窗
+      $sensitiveWordsLayer.show();
+    } else {
+      // 关闭弹窗
+      // 关闭敏感词列表弹窗
+      $sensitiveWordsLayer.hide();
+    }
+
+    sensitiveWordsLayerIsShow = !sensitiveWordsLayerIsShow;
+  })
+
+  /**
+   * 敏感词列表弹窗关闭按钮点击事件,关闭弹窗
+   **/
+  $('#sensitive-words-close-btn').on('click', function () {
+    var $sensitiveWordsLayer = $('#sensitive-words-layer');
+    // 清空pdf所有的高亮显示
+    removePdfHighlight();
+    // 关闭敏感词列表弹窗
+    $sensitiveWordsLayer.hide();
+    sensitiveWordsLayerIsShow = false;
+  })
 })
